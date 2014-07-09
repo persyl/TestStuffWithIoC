43c34c38f21a0f83a35a480b29bb8aa961d5be09
Per Lundkvist <per.lundkvist@di.se>
2014-07-09 05:03:21 +0200

    Small start of the Bull and Bear Game

---
+++ Solution/TestStuffWithIoC/Controllers/BullBearController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestStuffWithIoC.Controllers
+{
+    public class BullBearController : Controller
+    {
+        // GET: BullBear
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
