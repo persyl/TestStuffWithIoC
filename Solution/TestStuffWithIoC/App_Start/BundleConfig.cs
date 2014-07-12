144a81802dadf0fce8379ec19a7b944217f1d92b
Per Lundkvist <per.lundkvist@di.se>
2014-07-12 15:10:07 +0200

    Touch-punch fixes

--- Solution/TestStuffWithIoC/App_Start/BundleConfig.cs
+++ Solution/TestStuffWithIoC/App_Start/BundleConfig.cs
@@ -11,7 +11,9 @@ namespace TestStuffWithIoC.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                       "~/Scripts/jquery*"));
+                "~/Scripts/jquery*").Include("~/static/js/jquery*"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/touch-punch").Include("~/static/js/jquery.ui.touch-punch.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/static/styles.css")); //In Include clause you can define more files with comma, like: ,"~/Content/more.css"
