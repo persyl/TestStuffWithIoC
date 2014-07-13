052550a7457c0e3372535a5f071c43c0b0b03382
Per Lundkvist <per.lundkvist@di.se>
2014-07-13 08:09:08 +0200

    Moved BullBear js into a separate file

--- Solution/TestStuffWithIoC/App_Start/BundleConfig.cs
+++ Solution/TestStuffWithIoC/App_Start/BundleConfig.cs
@@ -15,6 +15,8 @@ namespace TestStuffWithIoC.App_Start
             
             bundles.Add(new ScriptBundle("~/bundles/touch-punch").Include("~/static/js/jquery.ui.touch-punch.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bullbear").Include("~/static/js/bullbear*"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/static/styles.css")); //In Include clause you can define more files with comma, like: ,"~/Content/more.css"
         }
