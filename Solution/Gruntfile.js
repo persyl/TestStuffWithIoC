180632ec4bb75bfbc4bb9eee652ca0aa3832b5a0
per.lundkvist <per.lundkvist@di.se>
2015-11-10 20:01:18 +0100

    Added Jasmine but no tests yet

--- Solution/Gruntfile.js
+++ Solution/Gruntfile.js
@@ -3,8 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ['Gruntfile.js', 
-	  'TestStuffWithIoC/static/js/**/*.js', 
-	  'test/**/*.js'
+	  'TestStuffWithIoC/static/js/**/*.js'
 	  ],
       options: {
         globals: {
@@ -13,26 +12,39 @@ module.exports = function(grunt) {
       }
     },
 	uglify: {
-		my_target: {
-		  files: {
-			'TestStuffWithIoC/static/js/dist/output.min.js': [
-				'TestStuffWithIoC/static/js/vendor/jquery/dist/jquery.js',
-				'TestStuffWithIoC/static/js/vendor/jquery-ui/jquery-ui.js',
-				'TestStuffWithIoC/static/js/vendor/signalr/jquery.signalR.js',
-				'TestStuffWithIoC/static/js/bullbear.js', 
-				'TestStuffWithIoC/static/js/jquery.ui.touch-punch.min.js'
-				]
-		  }
-		}
+  		my_target: {
+  		  files: {
+  			'TestStuffWithIoC/static/js/dist/output.min.js': [
+  				'TestStuffWithIoC/static/js/vendor/jquery/dist/jquery.js',
+  				'TestStuffWithIoC/static/js/vendor/jquery-ui/jquery-ui.js',
+  				'TestStuffWithIoC/static/js/vendor/signalr/jquery.signalR.js',
+  				'TestStuffWithIoC/static/js/bullbear.js', 
+  				'TestStuffWithIoC/static/js/jquery.ui.touch-punch.min.js'
+  				]
+  		  }
+  		}
 	  },
+    jasmine: {
+            all: {
+                src: [
+                    'TestStuffWithIoC/static/js/*.js',
+                    '!TestStuffWithIoC/static/js/jquery*.js'
+                ],
+                options: {
+                    'vendor': ['TestStuffWithIoC/static/js/vendor', 'TestStuffWithIoC/Scripts'],
+                    'specs': 'TestStuffWithIoC/static/js/spec/**/*.js'
+                }
+            }
+        },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
+      tasks: ['jshint', 'jasmine:all']
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint', 'uglify']);
